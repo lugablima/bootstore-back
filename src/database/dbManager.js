405cab80b131fdc0562430233abf80798a825def
ayrtonporto <22572595+Ikusa0@users.noreply.github.com>
2022-07-07 12:24:29 -0300

    chore: all database related functions are now inside dbManager.js

---
+++ src/database/dbManager.js
@@ -0,0 +1,40 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { db } from "./mongodb.js";
+
+const DAYS_10 = 60 * 60 * 24 * 10;
+
+export async function findUser(query) {
+  const user = await db.collection("users").findOne(query);
+  return user;
+}
+
+export async function createUser(user) {
+  const newUser = { ...user };
+  delete newUser.confirmation;
+
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(user.password, salt);
+
+  newUser.password = hashedPassword;
+
+  await db.collection("users").insertOne(newUser);
+}
+
+export async function createSession(userId) {
+  const secret = process.env.JWT_SECRET;
+  const exp = Date.now() / 1000 + DAYS_10;
+  const token = jwt.sign({ userId, exp }, secret);
+  const newSession = {
+    userId,
+    token,
+  };
+
+  await db.collection("sessions").insertOne(newSession);
+  return token;
+}
+
+export async function findSession(token) {
+  const user = await db.collection("sessions").findOne({ token });
+  return user;
+}
