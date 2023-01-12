670a25276a045f49790a33dba1003e82bb89a5c6
Lucas <lugablima@gmail.com>
2023-01-12 17:09:45 -0300

    refactor: add authentication middleware call before card and order routes

--- src/routes/cardsRouter.js
+++ src/routes/cardsRouter.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
-import validateToken from "../middlewares/authenticationMiddleware.js";
 import validateCard from "../middlewares/validateCardMiddleware.js";
 import { getCards, createNewCard, deleteCard } from "../controllers/cardController.js";
 
 const cardsRouter = Router();
 
-cardsRouter.get("/cards", validateToken, getCards);
-cardsRouter.post("/cards", validateToken, validateCard, createNewCard);
-cardsRouter.delete("/cards/:cardId", validateToken, deleteCard);
+cardsRouter.get("/cards", getCards);
+cardsRouter.post("/cards", validateCard, createNewCard);
+cardsRouter.delete("/cards/:cardId", deleteCard);
 
 export default cardsRouter;
